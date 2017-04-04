a311127d23b2e9eba91ed41efbcd5cd997180abf
Wes Maldonado <wes@brokenbuild.com>
2017-04-04 11:45:39 -0700

    Seems to pass the test

--- keyboard-row/app/keyboard.rb
+++ keyboard-row/app/keyboard.rb
@@ -1,10 +1,19 @@
 # https://leetcode.com/problems/keyboard-row/#/description
 class Keyboard
-  def initialize(words)
-    @words = words.map{|w| w.downcase}
-    puts @words
+  attr :rows, :considered
+  def initialize(considered)
+    @considered = considered.map{|w| w.downcase}
+    @rows = ['qwertyuiop'.split(''), 'asdfghjkl'.split(''), 'zxcvbnm'.split('')]
   end
   def words
-    []
+          row_one = []
+          row_two = []
+          row_three = []
+    @considered.each do |word|
+            row_one << word if word.split('').all?{|letter| @rows[0].include?(letter)}
+            row_two << word if word.split('').all?{|letter| @rows[1].include?(letter)}
+            row_three << word if word.split('').all?{|letter| @rows[2].include?(letter)}
+    end
+    [row_one, row_two, row_three].flatten
   end
 end 
